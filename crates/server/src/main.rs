55917b417c548c9a753bd465d7669dfb83f64198
akazdayo <82073147+akazdayo@users.noreply.github.com>
2026-07-06 16:50:52 +0900

    feat: axumをsetup

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,1 +1,16 @@
-fn main() {}
+use axum::{Router, routing::get};
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let app = Router::new().route("/", get(health));
+
+    let listener = TcpListener::bind("127.0.0.1:3000").await?;
+    axum::serve(listener, app).await?;
+
+    Ok(())
+}
+
+async fn health() -> &'static str {
+    "ok"
+}
